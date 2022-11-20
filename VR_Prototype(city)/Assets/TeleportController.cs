8da3de2a2072e5693041e9cd9c1cd60a423a9c1a
Zakaria Abdisalam <47097630+zabdisalam@users.noreply.github.com>
2022-11-20 16:32:53 -0500

    Teleportation Manager V3.0(release)

---
+++ VR_Prototype(city)/Assets/TeleportController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
+
+public class TeleportController : MonoBehaviour
+{
+    public GameObject baseControllerGameObject;
+    public GameObject teleportationGameObject;
+
+    public InputActionReference teleportActivationReference;
+
+    [Space]
+    public UnityEvent onTeleportActivate;
+    public UnityEvent onTeleportCancel;
+
+    private void Start()
+    {
+        teleportActivationReference.action.performed += TeleportModeActivate;
+        teleportActivationReference.action.canceled += TeleportModeCancel;
+    }
+
+    private void TeleportModeActivate(InputAction.CallbackContext obj) => onTeleportActivate.Invoke();
+
+    private void TeleportModeCancel(InputAction.CallbackContext obj) => Invoke("DeactivateTeleporter", .1f);
+    
+    void DeactivateTeleporter() => onTeleportCancel.Invoke();
+}
