e220937d61e283297a3b8d074b333cf8ebd4d175
Zakaria Abdisalam <47097630+zabdisalam@users.noreply.github.com>
2022-11-22 16:03:13 -0500

    v1.0(release) Teleport-Freeze: added teleport guide - which disappears once player teleports to it and disables teleportation.

--- VR_Prototype(city)/Assets/TeleportController.cs
+++ VR_Prototype(city)/Assets/TeleportController.cs
@@ -9,21 +9,45 @@ public class TeleportController : MonoBehaviour
     public GameObject baseControllerGameObject;
     public GameObject teleportationGameObject;
 
-    public InputActionReference teleportActivationReference;
+    static private bool hasTeleported = false;
+    public GameObject playerObj = null;
+    public GameObject teleportReticle = null;
 
+    public InputActionReference teleportActivationReference;
     [Space]
     public UnityEvent onTeleportActivate;
     public UnityEvent onTeleportCancel;
 
     private void Start()
     {
+        if (playerObj == null)
+        {
+            playerObj = GameObject.Find("XR Origin");
+            teleportReticle = GameObject.Find("SimpleTeleportTarget_circle_3");
+        }
         teleportActivationReference.action.performed += TeleportModeActivate;
         teleportActivationReference.action.canceled += TeleportModeCancel;
     }
+    private void Update()
+    {
+        Debug.Log("Player Position: X = " + playerObj.transform.position.x + " --- Y = " + playerObj.transform.position.y + " --- Z = " +
+         playerObj.transform.position.z);
+        if ((playerObj.transform.position.x >= -290.4 && -288.4 >= playerObj.transform.position.x) && (playerObj.transform.position.y >= 68.86 && 70.86 >= playerObj.transform.position.y))
+        {
+            teleportReticle.SetActive(false);
+            hasTeleported = true;
+        }
+    }
 
-    private void TeleportModeActivate(InputAction.CallbackContext obj) => onTeleportActivate.Invoke();
+    private void TeleportModeActivate(InputAction.CallbackContext obj)
+    {
+        if (!hasTeleported)
+            onTeleportActivate.Invoke();
+    }
 
     private void TeleportModeCancel(InputAction.CallbackContext obj) => Invoke("DeactivateTeleporter", .1f);
-    
+
     void DeactivateTeleporter() => onTeleportCancel.Invoke();
 }
+
+//-289.4 -> -288.4
