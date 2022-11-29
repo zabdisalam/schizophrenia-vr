dd497c364b0035dfa42cdb2a3142bba919d10d0a
Samuel Braun <samuelbraun17@gmail.com>
2022-11-28 21:49:07 -0500

    finishing dialogue branch

--- VR_Prototype(city)/Assets/HideDialogue.cs
+++ VR_Prototype(city)/Assets/HideDialogue.cs
@@ -6,17 +6,22 @@ public class HideDialogue : MonoBehaviour
 {
     public GameObject canvas;
     public GameObject playerObj;
+    public bool alreadyDone = false;
 
     void Start(){
         canvas.SetActive(false);
     }
 
     void Update(){
-        Debug.Log(playerObj.transform.position.x);
-        if (playerObj.transform.position.x <= -287){
-            canvas.SetActive(true);
+        // Debug.Log(playerObj.transform.position.x);
+        if ((playerObj.transform.position.x <= -287) && (alreadyDone == false)){
+            StartCoroutine(waiter());
+            alreadyDone = true;
         }
-        
+    }
 
+    IEnumerator waiter() {
+        yield return new WaitForSeconds(5);
+        canvas.SetActive(true);
     }
 }
--- VR_Prototype(city)/Assets/PlayAudioClips.cs
+++ VR_Prototype(city)/Assets/PlayAudioClips.cs
@@ -6,8 +6,11 @@ public class PlayAudioClips : MonoBehaviour
 {
 
     public AudioSource audio;
+    public AudioClip clip;
+    public GameObject canvas;
 
     public void playButton(){
-        audio.Play();
+        audio.PlayOneShot(clip);
+        canvas.transform.localScale = new Vector3(0, 0, 0);
     }
 }
